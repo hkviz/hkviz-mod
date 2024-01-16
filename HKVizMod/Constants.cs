91c7643cdf0c7116fa19965d6dc4736daf7b0ca9
Oliver Grack <oliver.grack@gmail.com>
2024-01-16 23:48:41 +0100

    github auto assign

--- HKVizMod/Constants.cs
+++ HKVizMod/Constants.cs
@@ -3,10 +3,10 @@
         public static string WEBSITE_DISPLAY_LINK = "hkviz.org";
         public static string WEBSITE_LINK = "https://www.hkviz.org";
 
-        public static string API_URL = "https://www.hkviz.org/api/rest/";
-        public static string LOGIN_URL = "https://www.hkviz.org/ingameauth/";
+        //public static string API_URL = "https://www.hkviz.org/api/rest/";
+        //public static string LOGIN_URL = "https://www.hkviz.org/ingameauth/";
 
-        //public static string API_URL = "http://localhost:3000/api/rest/";
-        //public static string LOGIN_URL = "http://localhost:3000/ingameauth/";
+        public static string API_URL = "http://localhost:3000/api/rest/";
+        public static string LOGIN_URL = "http://localhost:3000/ingameauth/";
     }
 }
