33348925b0999860aceda7626c950fa84bd41a31
Oliver Grack <oliver.grack@gmail.com>
2024-01-18 17:28:30 +0100

    constants point to hkviz.org

--- HKVizMod/Constants.cs
+++ HKVizMod/Constants.cs
@@ -3,11 +3,11 @@
         public static string WEBSITE_DISPLAY_LINK = "hkviz.org";
         public static string WEBSITE_LINK = "https://www.hkviz.org";
 
-        //public static string API_URL = "https://www.hkviz.org/api/rest/";
-        //public static string LOGIN_URL = "https://www.hkviz.org/ingameauth/";
+        public static string API_URL = "https://www.hkviz.org/api/rest/";
+        public static string LOGIN_URL = "https://www.hkviz.org/ingameauth/";
 
-        public static string API_URL = "http://localhost:3000/api/rest/";
-        public static string LOGIN_URL = "http://localhost:3000/ingameauth/";
+        //public static string API_URL = "http://localhost:3000/api/rest/";
+        //public static string LOGIN_URL = "http://localhost:3000/ingameauth/";
 
         public static string GetVersion() => typeof(Constants).Assembly.GetName().Version.ToString();
     }
